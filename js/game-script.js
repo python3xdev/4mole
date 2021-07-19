a2705f336fef0810025577b5269b0fbca5e04823
python3xdev <python3.x_dev@protonmail.com>
2021-07-19 14:42:11 +0300

    added start and reset game buttons

--- js/game-script.js
+++ js/game-script.js
@@ -1,11 +1,20 @@
 const holes = document.querySelectorAll(".hole");
 const scoreBoard = document.querySelector(".score");
+const totalScoreBoard = document.querySelector(".total-peeks");
+const accuracy = document.querySelector(".accuracy");
 const moles = document.querySelectorAll(".mole");
 const audioContainer = document.getElementById("audioContainer");
+const reloadButton = document.querySelector("#reload");
+
+var running = false
 
 let lastHole;
 let timeUp = false;
 let score = 0;
+
+function reload() {
+    reload = location.reload();
+}
 
 function randomTime(min, max) {
 	return Math.round(Math.random() * (max - min) + min);
@@ -25,16 +34,23 @@ function peep() {
 	const time = randomTime(600, 1000);
 	const hole = randomHole(holes);
 	hole.classList.add("up");
+	total_peeks++;
+	totalScoreBoard.textContent = total_peeks;
+	accuracy.textContent = Math.round(scoreBoard.textContent / totalScoreBoard.textContent * 100) + "%";
 	setTimeout(() => {
 		hole.classList.remove("up");
 		if (!timeUp) peep();
 	}, time);
+
 }
 
 function startGame() {
+	if (running) return;
 	scoreBoard.textContent = 0;
 	timeUp = false;
 	score = 0;
+	total_peeks = 0
+	running = true
 	peep();
 	// setTimeout(() => (timeUp = true), 10000); // This will run the program for 10 seconds, this is commented out so the program runs infinitely.
 }
@@ -52,3 +68,4 @@ function playHitSound() {
 }
 
 moles.forEach(mole => mole.addEventListener("click", bonk));
+reloadButton.addEventListener("click", reload, false);
